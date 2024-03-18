5dbe4eb1ace4346c777bc5f12d6e6009457948a5
Karmishs <vannghia943@gmail.com>
2024-03-18 21:47:13 +0700

    Update feature >> authentication: fix throw exception

--- src/feature/auth/sign_in/sign_in.service.ts
+++ src/feature/auth/sign_in/sign_in.service.ts
@@ -4,21 +4,23 @@ import { User } from 'src/models/user/interfaces/user.interface';
 import * as argon from 'argon2';
 import { Model } from 'mongoose';
 import { validateOrReject, ValidationError } from 'class-validator';
-import { UserNotFoundException, InvalidPasswordException, ValidationFailedException } from '../../../common/exceptions/exceptions';
 import {JwtModule, JwtService} from "@nestjs/jwt";
 import * as uuid from 'uuid';
 import {
     UserNotFoundException,
     InvalidPasswordException,
     ValidationFailedException,
-} from '../../../common/exceptions/exceptions';
+}
+from '../../../common/exceptions/exceptions';
 import Logger, { LoggerKey } from 'src/core/logger/interfaces/logger.interface';
 
 @Injectable()
 export class SignInService {
+
     constructor(
         @Inject('USER_MODEL') private readonly User: Model<User>,
         @Inject(LoggerKey) private logger: Logger,
+        private jwtService: JwtService
     ) {}
 
     async signIn(_signInDto: signInDto): Promise<any> {
@@ -34,19 +36,24 @@ export class SignInService {
             }
         }
 
-        const user = await this.User.findOne({ email: _signInDto.email }).select('+password');
+        try{
+            const user = await this.User.findOne({ email: _signInDto.email }).select('+password');
 
-        if (!user) {
-            throw new UserNotFoundException('User not found for email: ' + _signInDto.email);
+            if (!user) {
+                throw new UserNotFoundException('User not found for email: ' + _signInDto.email);
+            }
+
+            const isPasswordMatch = await argon.verify(user.password, _signInDto.password);
+
+            if (!isPasswordMatch) {
+                throw new InvalidPasswordException('Invalid password for user: ' + user.email);
+            }
+            const signToken = this.signToken(user.id, user.email);
+            return { message: 'Login successful',access_token: signToken };
+        } catch (error) {
+            this.logger.error('Error while signing in user', { error });
+            return error;
         }
-
-        const isPasswordMatch = await argon.verify(user.password, _signInDto.password);
-
-        if (!isPasswordMatch) {
-            throw new InvalidPasswordException('Invalid password for user: ' + user.email);
-        }
-        const signToken = this.signToken(user.id, user.email);
-        return { message: 'Login successful',access_token: signToken };
     }
 
     signToken(id: string, email: string) {
