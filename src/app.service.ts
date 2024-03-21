57df0f741fa4377292606f936f8a80fce9e4bc7c
Nguyễn Tiến Lợi <ntloi120202@gmail.com>
2024-03-22 01:19:44 +0700

    Update >> fix no set ttl in redis-cache

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,20 +1,19 @@
 import { Inject, Injectable } from '@nestjs/common';
 import Logger, { LoggerKey } from './core/logger/interfaces/logger.interface';
-import { BadRequestException } from './common/exceptions/exceptions';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
-import { Cache } from 'cache-manager';
+import { CacheStore } from '@nestjs/cache-manager';
 
 @Injectable()
 export class AppService {
     constructor(
         @Inject(LoggerKey) private logger: Logger,
         private configService: ConfigService<ConfigModule>,
-        @Inject(CACHE_MANAGER) private cacheManager: Cache,
+        @Inject(CACHE_MANAGER) private cacheManager: CacheStore,
     ) {}
 
     async getHello(): Promise<object> {
-        await this.cacheManager.set('key', '11111');
+        await this.cacheManager.set('key', '22222', {ttl: 100} );
 
         const value = await this.cacheManager.get('key');
         this.logger.info('Cache', { props: { key: value } });
