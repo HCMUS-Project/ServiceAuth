cde2ce78b5e14c90b1b8e38d0b37d598d3f63f8b
Nguyễn Tiến Lợi <ntloi120202@gmail.com>
2024-03-22 01:23:31 +0700

    Docs >> read comment use to redis

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,11 @@
 import { Inject, Injectable } from '@nestjs/common';
 import Logger, { LoggerKey } from './core/logger/interfaces/logger.interface';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+
+/**
+ * Import the CACHE_MANAGER token from the @nestjs/cache package
+ * Import the CacheStore interface from the @nestjs/cache package
+ * */
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { CacheStore } from '@nestjs/cache-manager';
 
@@ -13,9 +18,15 @@ export class AppService {
     ) {}
 
     async getHello(): Promise<object> {
+        // Set a key/value pair in the cache and  set ttl to 100 seconds
         await this.cacheManager.set('key', '22222', {ttl: 100} );
 
+        // Set a key/value pair in the cache and no ttl
+        await this.cacheManager.set("key2", "value2");
+
+        // Get a key/value pair from the cache
         const value = await this.cacheManager.get('key');
+
         this.logger.info('Cache', { props: { key: value } });
         
         const port = this.configService.get<number>('port');
