5006227c8f7fee921b3597cfad86a92fa603ca52
nghia <nghia@enet.io>
2024-07-11 14:49:02 +0700

    feat: add forgot password

--- src/feature/auth/verify_account/interface/verify_account.interface.ts
+++ src/feature/auth/verify_account/interface/verify_account.interface.ts
@@ -1,4 +1,6 @@
 import {
+    ForgotPasswordRequest,
+    ForgotPasswordResponse,
     SendMailRequest,
     SendMailResponse,
     VerifyAccountRequest,
@@ -10,3 +12,6 @@ export interface IVerifyAccountResponse extends VerifyAccountResponse.AsObject {
 
 export interface ISendMailRequest extends SendMailRequest.AsObject {}
 export interface ISendMailResponse extends SendMailResponse.AsObject {}
+
+export interface IForgotPasswordRequest extends ForgotPasswordRequest.AsObject {}
+export interface IForgotPasswordResponse extends ForgotPasswordResponse.AsObject {}
--- src/feature/auth/verify_account/verify_account.controller.ts
+++ src/feature/auth/verify_account/verify_account.controller.ts
@@ -2,6 +2,8 @@ import { Controller } from '@nestjs/common';
 import { GrpcMethod } from '@nestjs/microservices';
 import { VerifyAccountService } from './verify_account.service';
 import {
+    IForgotPasswordRequest,
+    IForgotPasswordResponse,
     ISendMailRequest,
     ISendMailResponse,
     IVerifyAccountRequest,
@@ -20,5 +22,16 @@ export class VerifyAccountController {
     @GrpcMethod('VerifyAccountService', 'SendMailOtp')
     async sendMailOtp(data: ISendMailRequest): Promise<ISendMailResponse> {
         return await this.verifyAccountService.sendMailOtp(data);
+    }
+
+
+    @GrpcMethod('VerifyAccountService', 'ForgotPassword')
+    async forgotPassword(data: IForgotPasswordRequest): Promise<IForgotPasswordResponse> {
+        return await this.verifyAccountService.forgotPassword(data);
+    }
+
+    @GrpcMethod('VerifyAccountService', 'sendMailForgotPassword')
+    async sendEmailForgotPassword(data: ISendMailRequest): Promise<ISendMailResponse> {
+        return await this.verifyAccountService.sendMailForgotPassword(data);
     }
 }
